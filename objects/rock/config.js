1338fbceb2ff97657dfcdfe625718ba1fa95cd13
Ryan Kubik <rkubik@twilio.com>
2021-08-31 18:28:39 -0700

    Clean up objects and create van possum

--- objects/rock/config.js
+++ objects/rock/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  animations: {},
   spriteSheets: {
     tq_python_rock: {
       fileName: "rock.png",
@@ -9,23 +8,14 @@ module.exports = {
       },
     },
   },
-  events: {
-    onPlayerDidInteract: (self, event, world) => {
-      if (self === event.target) {
-      }
-    },
-  },
   properties: {
     sprite: {
       defaultFrameIndex: 0,
       spriteSheet: "tq_python_rock",
-      layers: [],
     },
   },
 };
 
 // TODO:
-// Why does the temple_entry map crash upon loading
-// set up local_config to load directly in there for quicker testing!
 // create van possom object
 // create ghost people objects
