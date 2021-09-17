b46e4a9f91556ea6bf80dfee64c715a9903c2b6f
Ryan Kubik <rkubik@twilio.com>
2021-09-16 18:41:35 -0700

    Add onboarding arrows

--- levels/python/events.js
+++ levels/python/events.js
@@ -1,3 +1,28 @@
+function toggleDisplay(event, world, pystate) {
+  if (!pystate.hasSeenStaffChest) {
+    world.showEntities("arrow_staff", 0);
+  } else {
+    world.hideEntities("arrow_staff");
+  }
+
+  if (
+    pystate.hasSeenStaffChest &&
+    !pystate.hasSeenFirstNote &&
+    !pystate.clues.one
+  ) {
+    world.showEntities("arrow_note_one", 0);
+  } else {
+    world.hideEntities("arrow_note_one");
+  }
+
+  // See if there are any collected research notes that need to get hidden
+  world.destroyEntities((entity) => {
+    const { researchClueNumber } = entity.instance;
+
+    return researchClueNumber && pystate.clues[researchClueNumber];
+  });
+}
+
 // Default Python world state
 const DEFAULT_WORLD_STATE = {
   hasSeenStaffChest: false,
@@ -10,6 +35,19 @@ const DEFAULT_WORLD_STATE = {
 module.exports = function (event, world) {
   // Get current state of the python world
   const pystate = world.getState("pythonWorldState") || DEFAULT_WORLD_STATE;
+
+  if (event.name === "playerDidInteract") {
+    if (event.target.objectiveName === "python_basics_final") {
+      pystate.hasSeenStaffChest = true;
+    }
+
+    if (event.target.firstResearchClue) {
+      pystate.hasSeenFirstNote = true;
+    }
+  }
+
+  // Handle display state of indicator arrows and clues
+  toggleDisplay(event, world, pystate);
 
   // Update world state with convenience flag
   pystate.canSolvePuzzle = pystate.cluesFound === pystate.cluesTotal;
